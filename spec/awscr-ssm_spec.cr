a30c0c67a327bb87622329bff103bc982bbc349b
y2k2mt <y2.k2mt@gmail.com>
2018-11-19 23:50:33 +0900

    Introduce ameba

--- spec/awscr-ssm_spec.cr
+++ spec/awscr-ssm_spec.cr
@@ -5,7 +5,7 @@ describe Awscr::SSM do
   cli = Awscr::SSM::Client.new("ap-northeast-1")
 
   it "Plain text parameter" do
-    version = cli.put_parameter("foo", "bar")
+    cli.put_parameter("foo", "bar")
     actual = cli.get_parameter("foo")
     actual.should eq("bar")
     cli.delete_parameter("foo")
@@ -17,7 +17,7 @@ describe Awscr::SSM do
   end
 
   it "Secure string parameter" do
-    version = cli.put_parameter("foo", "bar", true)
+    cli.put_parameter("foo", "bar", true)
     actual = cli.get_parameter("foo", true)
     actual.should eq("bar")
     cli.delete_parameter("foo")
